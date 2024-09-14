24982f631cc865cfcb9531b6f671e719164db729
TramVo276 <tvo941352@gmail.com>
2024-09-14 05:47:57 +0000

    homework

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,6 +2,20 @@ public class GradeCalculator
 {
   public static void main(String[] args)
   {
-    // insert code here
+    String course = "AP Computer Science A"
+    int AmountTime = ((50 *4) + 37);
+    System.out.println("Average time spent in a week for this course in minues is: " + AmountTime + " mintunes.");
+    int HomeWork = 50;
+    int HW2 = 90;
+    int HW3 = 95;
+    int HW4 = 99;
+    int HW5 = 100;
+    System.out.println("Homework grades for this course is:");
+    System.out.println(HomeWork);
+    System.out.println(HW2);
+    System.out.println(HW3);
+    System.out.println(HW4);
+    System.out.println(HW5);
+    
   }
 }
